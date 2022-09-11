6aa3d30e7c0461e8deed2364db2d51287cc6ab8a
Ravyendra Singh <112679143+multiverseofShiv@users.noreply.github.com>
2022-09-11 19:26:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Navbar from './Component/Navbar';
+import News from './Component/News';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+            <Router>
+                  <Navbar/>
+                  <Switch>
+                      <Route exact path="/Home"><News key="1"   pageSize={6}  country = "in"  category = "general"/></Route>
+                      <Route exact path="/Business"><News key="2"  pageSize={6}  country = "in"  category = "buisness"/></Route>
+                      <Route exact path="/Entertainment"><News key="entertainement"  pageSize={6}  country = "in"  category = "entertainement"/></Route>
+                      <Route exact path="/Health"><News key="health" pageSize={6}  country = "in"  category = "health"/></Route>
+                      <Route exact path="/Science"><News key="science" pageSize={6}  country = "in"  category = "science"/></Route>
+                      <Route path="/Sports"><News key="sports" pageSize={6}  country = "in"  category = "sports"/></Route>
+                      <Route exact path="/Technology"><News key="technology" pageSize={6}  country = "in"  category = "technology"/></Route>
+                  </Switch>
+            </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
+
